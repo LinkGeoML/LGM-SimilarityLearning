19dca3941fe773ddb88b8d5673b332a806cd8e5d
George Perakis <perakisgeorgios@gmail.com>
2019-12-18 23:27:30 +0200

    Add custom metrics to metric.py

---
+++ similarity_learning/metric.py
@@ -0,0 +1,36 @@
+import numpy as np
+from tensorflow.keras import backend as K
+
+
+def compute_accuracy(y_true, y_pred):
+    """
+    Compute classification accuracy with a fixed threshold on distances.
+    Parameters
+    ----------
+    y_true
+    y_pred
+
+    Returns
+    -------
+
+    """
+    '''
+    '''
+    pred = y_pred.ravel() < 0.5
+    return np.mean(pred == y_true)
+
+
+def accuracy(y_true, y_pred):
+    """
+    Compute classification accuracy with a fixed threshold on distances.
+
+    Parameters
+    ----------
+    y_true
+    y_pred
+
+    Returns
+    -------
+
+    """
+    return K.mean(K.equal(y_true, K.cast(y_pred < 0.5, y_true.dtype)))
