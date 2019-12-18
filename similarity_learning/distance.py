9ddee97b0588f0ae8f1bfad1f77e022a2aef1671
George Perakis <perakisgeorgios@gmail.com>
2019-12-18 23:25:05 +0200

    Add euclidean_distance to distance.py

--- similarity_learning/distance.py
+++ similarity_learning/distance.py
@@ -17,3 +17,14 @@ def l1_distance(tensors):
     :return:
     """
     return K.abs(tensors[0] - tensors[1])
+
+
+def euclidean_distance(tensors):
+    x, y = tensors
+    sum_square = K.sum(K.square(x - y), axis=1, keepdims=True)
+    return K.sqrt(K.maximum(sum_square, K.epsilon()))
+
+
+def eucl_dist_output_shape(shapes):
+    shape1, shape2 = shapes
+    return (shape1[0], 1)
