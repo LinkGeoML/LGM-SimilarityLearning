11b31d2e21e4691486e8769dc5353928461c4cb7
George Perakis <perakisgeorgios@gmail.com>
2019-12-20 00:29:45 +0200

    config.py : General Refactoring

--- similarity_learning/config.py
+++ similarity_learning/config.py
@@ -2,8 +2,9 @@
 Configuration Module
 """
 import os
-from distutils.util import strtobool
 from typing import Any
+
+from distutils.util import strtobool
 
 ENV_PREFIX = "LGM_"
 
@@ -36,11 +37,6 @@ def parse_bool(value) -> bool:
     return bool(strtobool(value))
 
 
-# This should be one of 'development', 'test'
-RUN_ENV = _env_get('RUN_ENV', default='development')
-assert RUN_ENV in ['development', 'test']
-
-
 class DirConf:
     DATA_DIR = 'data'
     MODELS_DIR = 'models'
@@ -48,55 +44,7 @@ class DirConf:
     LOG_DIR = 'log'
 
 
-class ModelConf:
-    """Model Training Configuration class"""
-
-    SEED = 42
-    EPOCHS = 10
-
-    # Default dataset names for each task
-    DS_TRAIN = 'train.csv'
-    DS_VAL = 'val.csv'
-    DS_TEST = 'test.csv'
-
-    # Default embedding vector dimensions
-    EMBEDDINGS_DIM = 128
-
-    # Default name for whole dataset embeddings
-    EMBEDDINGS_FNAME = 'embeddings.npy'
-
-    # Default Batch Size
-    DEFAULT_BS = 32
-
-    # # NOTE: Using the env var CUDA_VISIBLE_DEVICES one can filter the list
-    # # of available CUDA devices!
-    # if torch.cuda.is_available():
-    #     TORCH_DEVICE = torch.device("cuda")
-    # else:
-    #     TORCH_DEVICE = torch.device("cpu")
-    #
-    # TORCH_CUDA_DEVICES_CNT = torch.cuda.device_count()
-
-    # Trained models
-    EMBEDDINGS_MODEL_NAME = _env_get(
-        'EMBEDDINGS_MODEL', default='test_model')
-
-    MODEL_NUM_CLASSES = 2
-
-
 # === LOGGING ===
-if RUN_ENV == 'development':
-    root_handlers = ['devlog']
-    root_level = 'INFO'
-
-elif RUN_ENV == 'test':
-    root_handlers = ['devlog']
-    root_level = 'INFO'
-
-else:
-    root_handlers = ['console', 'file']
-    # This can be set in a more fine grained way per handler.
-    root_level = 'INFO'
 
 
 class LoggerConf:
@@ -104,28 +52,16 @@ class LoggerConf:
 
     LOG_DIR = DirConf.LOG_DIR
 
-    APP_LOG_DIR = os.path.join(LOG_DIR, 'app')
-
     EXPERIMENT_LOG_DIR = os.path.join(LOG_DIR, 'experiment')
 
     if not os.path.exists(LOG_DIR):
         os.mkdir(LOG_DIR)
 
-    if not os.path.exists(APP_LOG_DIR):
-        os.mkdir(APP_LOG_DIR)
-
     if not os.path.exists(EXPERIMENT_LOG_DIR):
         os.mkdir(EXPERIMENT_LOG_DIR)
 
-    # Training defaults
-    LOG_INTERVAL = 20
-
     # This will be necessary for syslog (if used)
-    LOGGER_NAME = 'beholder'
-
-    LOG_NAMES = dict(development='development.log',
-                     test='test.log',
-                     production='production.log')
+    LOGGER_NAME = 'LGM_Similarity_Learning'
 
     # Logger configuration
     LOGGING_CONF = {
@@ -148,22 +84,15 @@ class LoggerConf:
             'devlog': {
                 'level': 'DEBUG',
                 'formatter': 'devlog',
-                'filename': os.path.join(APP_LOG_DIR, LOG_NAMES[RUN_ENV]),
+                'filename': os.path.join(
+                    EXPERIMENT_LOG_DIR, 'development.log'),
                 'class': 'logging.handlers.RotatingFileHandler'
             }
         },
         'loggers': {
             LOGGER_NAME: {
-                'level': root_level,
-                'handlers': root_handlers
+                'level': 'INFO',
+                'handlers': ['devlog', 'console']
             },
         },
-    }
-
-    EXP_LOG_CONF = {
-        'disable_existing_loggers': False,
-        'level': root_level,
-
-        'formatter': '%(asctime)s - PID:%(process)d - %(name)s.py:'
-                     '%(funcName)s:%(lineno)d - %(levelname)s - %(message)s',
     }
