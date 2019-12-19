9aba011c612495839cb57c482c1894eceac38dfa
George Perakis <perakisgeorgios@gmail.com>
2019-12-20 00:31:49 +0200

    metric.py : Rename accuracy metric to distance_accuracy

--- similarity_learning/metric.py
+++ similarity_learning/metric.py
@@ -2,25 +2,7 @@ import numpy as np
 from tensorflow.keras import backend as K
 
 
-def compute_accuracy(y_true, y_pred):
-    """
-    Compute classification accuracy with a fixed threshold on distances.
-    Parameters
-    ----------
-    y_true
-    y_pred
-
-    Returns
-    -------
-
-    """
-    '''
-    '''
-    pred = y_pred.ravel() < 0.5
-    return np.mean(pred == y_true)
-
-
-def accuracy(y_true, y_pred):
+def distance_accuracy(y_true, y_pred):
     """
     Compute classification accuracy with a fixed threshold on distances.
 
@@ -34,3 +16,19 @@ def accuracy(y_true, y_pred):
 
     """
     return K.mean(K.equal(y_true, K.cast(y_pred < 0.5, y_true.dtype)))
+
+
+def compute_accuracy(y_true, y_pred):
+    """
+    Compute classification accuracy with a fixed threshold on distances.
+    Parameters
+    ----------
+    y_true
+    y_pred
+
+    Returns
+    -------
+
+    """
+    pred = y_pred.ravel() < 0.5
+    return np.mean(pred == y_true)
